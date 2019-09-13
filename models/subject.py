8cbda8874fe2324550983719508ebf32e00607f0
Huu Lane <huulane@gmail.com>
2019-09-13 23:34:13 +0800

    实现基础功能, 以及通过测试

--- models/subject.py
+++ models/subject.py
@@ -1,5 +1,5 @@
 import json
-from models.mongo import insert, find, drop
+from models.mongo import insert, find, drop, find_by_id
 
 
 def load(path):
@@ -9,7 +9,7 @@ def load(path):
     try:
         with open(path, 'r', encoding='utf-8') as f:
             s = f.read()
-            print('读取课程数据')
+            print(f'读取{path}数据')
             return json.loads(s)
     except FileNotFoundError:
         # 默认 list
@@ -30,6 +30,8 @@ def week_day_to_num(day: str) -> int:
 
 
 class Subject:
+    _schooltime = []
+
     def __init__(self, **kwargs):
         '''
         格式:
@@ -57,21 +59,83 @@ class Subject:
         for d in ds:
             obj_id = insert('info', d)
             # 建立时间索引
-            # 格式为 十位: (星期几-1), 个位: 第几节
+            # 格式为 十位: (星期几-1), 个位: (第几节-1)
             for tp in d['time_place']:
-                i = week_day_to_num(tp[0]) * 10 + tp[1]
+                i = week_day_to_num(tp[0]) * 10 + tp[1] - 1
                 insert('timetable', dict(
                     i=i,
                     info=str(obj_id),
                 ))
 
     @classmethod
+    def schooltime(cls) -> list:
+        r = cls._schooltime
+        if len(r) > 0:
+            return r
+        # 第一次运行, 载入到内存
+        d = list(filter(lambda d: d.get('title', None) ==
+                        "夏季", load('static/routine.json')))[0]
+        r = d.get('schooltime')
+        cls._schooltime = r
+        return r
+
+    @classmethod
+    def fake_next(cls, day, clock) -> tuple:
+        '''
+        假设课程全满, 求出下一节课, 是今天第几节课或明天第一节课\n
+        返回 (星期几-1), (第几节课-1)
+        '''
+        class_ordinal = 0
+        for i, t in enumerate(cls.schooltime()):
+            if clock < t:
+                class_ordinal = i
+                break
+        else:
+            # 返回下一个工作日的第一节课..
+            day = day + 1
+            # 礼拜六或礼拜天就改为礼拜一
+            if (day + 1) == 6 or 7:
+                day = 0
+        return day, class_ordinal
+
+    @classmethod
+    def find_by_rank(cls, i) -> list:
+        '''
+        功能见 real_next
+        '''
+        r = list(find('timetable', {
+            'i': {"$gte": i}
+        }, mult=True, sort='i'))
+        if len(r) == 0:
+            r = list(find('timetable', {
+                'i': {"$gte": 0}
+            }, mult=True, sort='i'))
+        obj_id = r[0].get('info')
+        return find_by_id('info', obj_id)
+
+    @classmethod
+    def real_next(cls, day: int, class_ordinal: int):
+        '''
+        接收 '下一节课' (星期几-1, 第几节课-1)
+        将其转化为 课程排位(数据库索引), 寻找大于等于它的第一个存在
+        若不存在, 设课程排位为0, 重新搜索.
+        返回一个 含有课程信息的 dict
+        '''
+        # 转化成课程表时间索引
+        i = day * 10 + class_ordinal
+        print('课程排位', i)
+        return cls.find_by_rank(i)
+
+    @classmethod
     def next(cls, time: tuple):
         '''
         查看下一节课
-        time: 第几周/(星期几-1)/时/分
+        参数 time: 第几周/(星期几-1)/时/分
         '''
-        pass
+        # 现在时分
+        clock = time[2] * 100 + time[3]
+        f = cls.fake_next(time[1], clock)
+        return cls.real_next(*f)
 
     @classmethod
     def all(cls) -> list:
